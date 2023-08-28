03b84732da0a5fd8a6504a4556246736ec0c008f
J-C-L-G <ps_jclg@hotmail.com>
2023-08-27 18:57:49 -0700

    update mod 25

--- 2. Docker Images and Containers/25. Undertanding Image Layers/code/app.js
+++ 2. Docker Images and Containers/25. Undertanding Image Layers/code/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.json({success: true})
+  response.json({success: true, update: true})
 });
 
 app.listen(80);
--- legacy/25. Undertanding Image Layers/steps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# For each command (excluding CMD) Dockers creates a layer and caches it to build/reuse if nothing changed
-# in this example we can optimize and reuse the "npm install" step if we first copy the package into the
-# image, if the project dependencies didn't change docker will be able to re-use the cached version of npm install;
-# since most of the changes will happen in the source code, we can change the execution order and get speed up the build.
-
-docker build .
